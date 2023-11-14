78f95263c7775ce2a6a2dfb9101e0edd9b055ce9
Charlisteketee <charlisteketee@gmail.com>
2023-11-14 14:27:32 -0800

    Pass connection function into setUpInput

--- play.js
+++ play.js
@@ -1,31 +1,12 @@
 const net = require("net");
 // import connect function
 const { connect } = require("./client");
-/*
-client.setEncoding("utf8"); // interpret data as text
-client.on("data", (data) => {
-  console.log("Message from client: ", data);
-});
-*/
+const { setupInput } = require("./input");
 
-// setup interface to handle user input from stdin
+console.log("Connecting ...");
+const connection = connect();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-// check if the received key is the Ctrl+C combination (\u0003):
-// If it is, terminate the game using process.exit()
-const handleUserInput = function () {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+// pass the connection object to setUpInput
+setupInput(connection);
 
 module.exports = { setupInput };
